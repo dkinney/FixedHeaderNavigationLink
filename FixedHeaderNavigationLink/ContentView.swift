e977233864f854381ba56d77c740845be1a905ae
Dan Kinney <dan@tomesoftware.com>
2020-01-30 03:40:14 -0500

    Initial Commit

---
+++ FixedHeaderNavigationLink/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FixedHeaderNavigationLink
+//
+//  Created by Dan Kinney on 1/30/20.
+//  Copyright © 2020 Dan Kinney. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
